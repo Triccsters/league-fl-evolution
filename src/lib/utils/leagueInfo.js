c3929476f876fd486ba1634eaba5fa1f4c87aae5
T.J. Ricci <trickster1434@gmail.com>
2026-05-06 16:06:43 -0500

    Configure for FL Evolution (Dynasty Superflex 12-team)

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -1,8 +1,8 @@
 /*   STEP 1   */
-export const leagueID = "1253477414021902336"; // your league ID
-export const leagueName = "FL Players"; // your league name
+export const leagueID = "1314321039802507264"; // your league ID
+export const leagueName = "FL Evolution"; // your league name
 export const dues = 100; // (optional) used in template constitution page
-export const dynasty = false; // true for dynasty leagues, false for redraft and keeper
+export const dynasty = true; // true for dynasty leagues, false for redraft and keeper
 export const enableBlog = false; // requires VITE_CONTENTFUL_ACCESS_TOKEN and VITE_CONTENTFUL_SPACE environment variables
 
 /*   STEP 2   */
